007de50fb312904c3dd00c808e40ac0a19fe9c4e
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-25 20:48:04 +0800

    Initial Commit

---
+++ HHSuperAdmin/HHSuperAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  HHSuperAdminApp.swift
+//  HHSuperAdmin
+//
+//  Created by StartupBuilder.INFO on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct HHSuperAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
